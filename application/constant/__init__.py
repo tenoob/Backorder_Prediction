14d6028d7a8d67719f8b7d3512c71935096e1acc
tenoob <18bcs6218@gmail.com>
2022-10-21 16:05:25 +0530

    done with dataingestion and pipleine entities

--- application/constant/__init__.py
+++ application/constant/__init__.py
@@ -14,4 +14,16 @@ CONFIGRATION_FILE_PATH = os.path.join(
     CONFIGRATION_FILE_NAME)
 
 #Training pipeline related variable
-TRAINING_PIPELINE_CONFIG_KEY = 'training_'
+TRAINING_PIPELINE_CONFIG_KEY = 'training_pipeline_config'
+TRAINING_PIPELINE_NAME_KEY = 'pipeline_name'
+TRAINING_PIPELINE_ARTIFACT_DIR_KEY = 'artifact_dir'
+
+#Data Ingestion related variable
+DATA_INGESTION_CONFIG_KEY = 'data_ingestion_config'
+DATA_INGESTION_ARTIFACT_DIR = 'data_ingestion'
+DATA_INGESTION_RAW_DATA_DIR_KEY = 'raw_data_dir'
+DATA_INGESTION_DATASET_DOWNLOAD_URL_KEY = 'dataset_download_url'
+DATA_INTESTION_TGZ_DOWNLOAD_KEY = 'tgz_download_dir'
+DATA_INGESTION_INGESTED_DIR_KEY = 'ingested_dir'
+DATA_INGESTION_INGESTED_TRAIN_DIR_KEY = 'ingested_train_dir'
+DATA_INGESTION_INGESTED_TEST_DIR_KEY = 'ingested_test_dir'
