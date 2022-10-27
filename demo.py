f5fa85b4b70a97095d2df103aede9e4221daf656
tenoob <18bcs6218@gmail.com>
2022-10-28 01:26:12 +0530

    i am idiot

--- demo.py
+++ demo.py
@@ -15,7 +15,7 @@ def main():
         data_ingestion = DataIngestion(data_ingestion_config=Configration().get_data_ingestion_config())
         data_ingestion.initiate_data_ingestion()
     except Exception as e:
-        logger.error(f"{e}")
+        logging.error(f"{e}")
         print(e)
 
 if __name__=='__main__':
