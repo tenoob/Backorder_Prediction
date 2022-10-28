240afd45398c580361ab95bee7eedb7f37bd4931
tenoob <18bcs6218@gmail.com>
2022-10-29 01:38:34 +0530

    done with data validation entity

--- application/entity/config_entity.py
+++ application/entity/config_entity.py
@@ -4,3 +4,6 @@ TrainingPipelineCongif = namedtuple('TrainingPipelineConfig',['artifact_dir'])
 
 DataIngestionConfig = namedtuple('DataIngestionConfig',
 ['dataset_download_url','tgz_download_dir','raw_data_dir','ingested_train_dir','ingested_test_dir','train_file_name','test_file_name'])
+
+DataValidationConfig = namedtuple("DataValidationConfig",
+['schema_file_path','report_file_path','report_page_file_path'])
