79b5ec604e0476e5bddaa2de645283e748e79261
tenoob <18bcs6218@gmail.com>
2022-10-28 15:20:43 +0530

    updated data Ingestion config tuple

--- application/entity/config_entity.py
+++ application/entity/config_entity.py
@@ -3,4 +3,4 @@ from collections import namedtuple
 TrainingPipelineCongif = namedtuple('TrainingPipelineConfig',['artifact_dir'])
 
 DataIngestionConfig = namedtuple('DataIngestionConfig',
-['dataset_download_url','tgz_download_dir','raw_data_dir','ingested_train_dir','ingested_test_dir'])
+['dataset_download_url','tgz_download_dir','raw_data_dir','ingested_train_dir','ingested_test_dir','train_file_name','test_file_name'])
