fbc60c87636f26b577e69f760a41433e0542a424
tenoob <18bcs6218@gmail.com>
2022-11-05 20:15:05 +0530

    done with congfigration of model trainer

--- application/config/configration.py
+++ application/config/configration.py
@@ -2,7 +2,7 @@
 from application.constant import *
 from application.logger import logging
 from application.exception import BackorderException
-from application.entity.config_entity import DataIngestionConfig, DataValidationConfig, TrainingPipelineCongif , DataTransformationConfig
+from application.entity.config_entity import DataIngestionConfig, DataValidationConfig, TrainingPipelineCongif , DataTransformationConfig , ModelTrainerConfig
 from application.entity.artifact_entity import DataIngestionArtifact
 from application.util.utililty import read_yaml_file
 from urllib import response
@@ -152,6 +152,42 @@ class Configration:
             logging.info(f"Data Transformation Config: {data_transformation_config}")
 
             return data_transformation_config
+        except Exception as e:
+            raise BackorderException(e,sys) from e
+
+    def get_model_trainer_config(self) -> ModelTrainerConfig:
+        try:
+            atrifact_dir = self.training_pipeline_config.artifact_dir
+
+            model_trainer_artifact_dir = os.path.join(
+                atrifact_dir,
+                MODEL_TRAINER_ARTIFACT_DIR,
+                self.current_time_stamp
+            )
+
+            model_trainer_config_info = self.config_info[MODEL_TRAINER_CONFIG_KEY]
+
+            trained_model_file_path = os.path.join(
+                model_trainer_config_info[MODEL_TRAINER_TRAINED_MODEL_DIR_KEY],
+                model_trainer_config_info[MODEL_TRAINER_TRAINED_MODEL_FILE_NAME_KEY]
+            )
+
+            model_config_file_path = os.path.join(
+                model_trainer_config_info[MODEL_TRAINER_MODEL_CONFIG_DIR_KEY],
+                model_trainer_config_info[MODEL_TRAINER_MODEL_CONFIG_FILE_NAME_KEY]
+            )
+
+            base_accuracy = model_trainer_config_info[MODEL_TRAINER_BASE_ACCURACY_KEY]
+
+            model_trainer_config = ModelTrainerConfig(
+                trained_model_file_path=trained_model_file_path,
+                model_config_file_path=model_config_file_path,
+                base_accuracy=base_accuracy
+            )
+
+            logging.info(f"Model Trainer Config: {model_trainer_config}")
+
+            return model_trainer_config
         except Exception as e:
             raise BackorderException(e,sys) from e
     
--- application/constant/__init__.py
+++ application/constant/__init__.py
@@ -67,7 +67,7 @@ DATA_TRANSFORMATION_PREPROCESSING_OBJECT_FILE_NAME = 'preprocessed_object_file_n
 
 #Model Trainer related variable
 MODEL_TRAINER_ARTIFACT_DIR = 'model_trainer'
-MODEL_TRAINER_CONFIG_DIR = 'model_trainer_config'
+MODEL_TRAINER_CONFIG_KEY = 'model_trainer_config'
 MODEL_TRAINER_TRAINED_MODEL_DIR_KEY = 'trained_model_dir'
 MODEL_TRAINER_TRAINED_MODEL_FILE_NAME_KEY = 'model_file_name'
 MODEL_TRAINER_BASE_ACCURACY_KEY = 'base_accuracy'
