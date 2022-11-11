855c8c085eb5a060a9ddf99bcc48ef837e980c3b
tenoob <18bcs6218@gmail.com>
2022-11-11 16:34:15 +0530

    added code for data and other file load

--- application/util/utililty.py
+++ application/util/utililty.py
@@ -95,3 +95,52 @@ def load_numpy_array_data(file_path: str) -> np.array:
             return np.load(file_obj,allow_pickle=True)
     except Exception as e:
         raise BackorderException(e,sys) from e
+
+
+def get_sample_model_config_yaml_file(export_dir:str):
+    try:
+        model_config = {
+            GRID_SEARCH_KEY:{
+                MODULE_KEY: 'sklearn.model_selection',
+                CLASS_KEY: 'GridSearchCV',
+                PARAM_KEY: {
+                    'cv':5,
+                    'verbose':2
+                }
+            },
+            MODEL_SELECTION_KEY: {
+                'module_0':{
+                    MODULE_KEY:'module_of_model',
+                    CLASS_KEY: 'ModelClassName',
+                    PARAM_KEY:
+                    {
+                        'param_name1': 'value1',
+                        'param_name2':'value2',
+                    },
+                    SEARCH_PARAM_GRID_KEY:
+                    {
+                        'param_name':['param_value1','param_value2']
+                    }
+                },
+            }
+        }
+
+        os.makedirs(export_dir,exist_ok=True)
+        export_file_path = os.path.join(export_dir,"model.yaml")
+        with open(export_file_path,'w') as file:
+            yaml.dump(model_config,file)
+        return export_file_path
+    except Exception as e:
+        raise BackorderException(e,sys) from e
+
+
+def load_object(file_path:str):
+    """
+    file_path: str
+    """
+    try:
+        with open(file_path,'rb') as file_obj:
+            return dill.load(file_obj)
+
+    except Exception as e:
+        raise BackorderException(e,sys) from e
