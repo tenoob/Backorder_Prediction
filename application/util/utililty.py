4b51739ee6b3d4fd78ce64d0640705c485fd7857
tenoob <18bcs6218@gmail.com>
2022-11-16 15:45:24 +0530

    added module for writing yaml file

--- application/util/utililty.py
+++ application/util/utililty.py
@@ -144,3 +144,17 @@ def load_object(file_path:str):
 
     except Exception as e:
         raise BackorderException(e,sys) from e
+
+def write_yaml_file(file_path:str , data:dict = None):
+    """
+    Create yaml file
+    file_path: str
+    data: dict
+    """
+    try:
+        os.makedirs(os.path.dirname(file_path),exist_ok=True)
+        with open(file_path,'w') as yaml_file:
+            if data is not None:
+                yaml.dump(data,yaml_file)
+    except Exception as e:
+        raise BackorderException(e,sys) from e
