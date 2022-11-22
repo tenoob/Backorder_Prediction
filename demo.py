f54386826c981d1d3ebae697c93cb14877d7bbeb
tenoob <18bcs6218@gmail.com>
2022-11-22 15:22:14 +0530

    demo last update

--- demo.py
+++ demo.py
@@ -1,5 +1,6 @@
 
 from application.logger import logging
+from application.constant import CONFIGRATION_FILE_PATH
 from application.component.data_ingestion import DataIngestion
 from application.exception import BackorderException
 from application.config.configration import Configration
@@ -17,8 +18,14 @@ def main():
         """"data_ingestion = DataIngestion(data_ingestion_config=Configration().get_data_ingestion_config())
         data_ingestion.initiate_data_ingestion()"""
 
-        pipeline = Pipeline()
-        pipeline.run_pipeline()
+        """pipeline = Pipeline()
+        pipeline.run_pipeline()"""
+        config_path = os.path.join("config","config.yaml")
+        pipeline = Pipeline(Configration(
+            config_file_path=CONFIGRATION_FILE_PATH))
+
+        pipeline.start()
+
     except Exception as e:
         logging.error(f"{e}")
         print(e)
