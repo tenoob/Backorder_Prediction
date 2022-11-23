7bb573380a2c36d61efda4de7ebcd6d546524615
tenoob <18bcs6218@gmail.com>
2022-11-23 14:42:50 +0530

    methods for getting log file and df added

--- application/logger/__init__.py
+++ application/logger/__init__.py
@@ -1,10 +1,27 @@
-from distutils.log import INFO
-import imp
+import pandas as pd
 import logging
 import os
 from application.constant import LOG_DIR,CURRENT_TIME_STAMP
 
 LOG_FILE_NAME = f'log_{CURRENT_TIME_STAMP}.log'
+
+def get_log_file_name():
+    return f"log_{CURRENT_TIME_STAMP}.log"
+
+def get_log_dataframe(file_path):
+    data=[]
+    with open(file_path) as log_file:
+        for line in log_file.readlines():
+            data.append(line.split("^;"))
+
+    log_df = pd.DataFrame(data)
+    columns=["Time stamp","Log Level","line number","file name","function name","message"]
+    log_df.columns=columns
+    
+    log_df["log_message"] = log_df['Time stamp'].astype(str) +":$"+ log_df["message"]
+
+    return log_df[["log_message"]]
+
 
 os.makedirs(LOG_DIR,exist_ok=True)
 
