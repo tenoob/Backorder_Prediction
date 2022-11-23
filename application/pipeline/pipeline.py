a08cf535949ae8cf31c53cfd21833f702acb6f38
tenoob <18bcs6218@gmail.com>
2022-11-23 14:43:26 +0530

    rectified error

--- application/pipeline/pipeline.py
+++ application/pipeline/pipeline.py
@@ -231,7 +231,7 @@ class Pipeline(Thread):
             raise BackorderException(e,sys) from e
 
     @classmethod
-    def get_experiment_status(cls , limit: int=5) -> pd.DataFrame:
+    def get_experiments_status(cls , limit: int=5) -> pd.DataFrame:
         try:
             if os.path.exists(Pipeline.experiment_file_path):
                 df = pd.read_csv(Pipeline.experiment_file_path)
